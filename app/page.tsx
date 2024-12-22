269a9d55e818dc7f599243e02d574ae2d56968d6
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-12-22 12:23:52 +1000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>My Home Page</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
+}
+
+export default HomePage;
