2dcc12c3e22bc98e960a2385523ffd5ce0cb47b6
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-12-23 22:36:47 +1000

    67 Refactoring Input Logic into a Reusable Component

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,6 +1,5 @@
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+
 const createProfileAction = async (formData: FormData) => {
   'use server';
   const firstName = formData.get('firstName') as string;
