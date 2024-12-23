b51d5a82fea32d93a38b0d7d42af18ab76c51055
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-12-23 21:47:15 +1000

    65 Completing the Links Dropdown Component

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -11,35 +11,46 @@ import { Button } from '../ui/button';
 import UserIcon from './UserIcon';
 import { links } from '@/utils/links';
 import SignOutLink from './SignOutLink';
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 function LinksDropdown() {
-  // Return the JSX structure for the dropdown menu
   return (
     <DropdownMenu>
-      {/* Trigger for the dropdown menu, allowing it to be opened when the button is clicked */}
-      <DropdownMenuTrigger asChild>
-        {/* Button styled as an outline with flex layout and a maximum width */}
-        <Button variant='outline' className='flex gap-4 max-w-[100px]'>
-          {/* Icon representing alignment, with specified width and height */}
-          <LuAlignLeft className='w-6 h-6' />
+      <DropdownMenuTrigger>
+        <Button variant="outline" className="flex items-center">
           <UserIcon />
+          <LuAlignLeft className="ml-2" />
         </Button>
       </DropdownMenuTrigger>
-      {/* Content of the dropdown menu, with specified width and alignment */}
       <DropdownMenuContent className='w-52' align='start' sideOffset={10}>
-        {/* Map through the links array to create dropdown items */}
-        {links.map((link) => {
-          return (
-            // Each dropdown item is keyed by the link's href
-            <DropdownMenuItem key={link.href}>
-              {/* Link component styled to be capitalized and full width */}
-              <Link href={link.href} className='capitalize w-full'>
-                {/* Display the label of the link */}
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Login</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link href={link.href} className='capitalize w-full'>
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
