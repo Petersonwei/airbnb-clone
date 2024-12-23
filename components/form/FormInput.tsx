e84332ee19eb5bc8fb6a89650fe77ff6dd0d37b2
Petersonwei <140482566+Petersonwei@users.noreply.github.com>
2024-12-23 23:11:28 +1000

    68  Creating a Reusable SubmitButton Component with useFormStatus

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -1,9 +1,6 @@
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
-
 type FormInputProps = {
   name: string;
   type: string;
